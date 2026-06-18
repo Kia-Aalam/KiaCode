c5becc7255cde73e86cce7bf7a0e80a1f2246eb4
Kia Aalam <kiaaalam7@gmail.com>
2026-06-18 20:04:54 +0330

    fix send message and add dynamic name course in footer

--- home_app/views.py
+++ home_app/views.py
@@ -1,19 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from home_app.models import Footer
+from courses_app.models import Course
 
 def base_page(request):
-    return render(request, 'base.html')
+    # course list
+    courses = Course.objects.all()
+    
+    # message
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        message = request.POST.get('message')
+        Footer.objects.create(username=username, message=message)
+        return redirect('home')
+    
+    return render(request, 'base.html', context={'courses': courses})
 
 def about_me(request):
     return render(request, 'about_me.html')
 
 def contact(request):
     return render(request, 'contact.html')
-
-def footer(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        message = request.POST.get('message')
-        Footer.objects.create(username=username, message=message)
-
-    return render(request, 'includes/footer.html')
