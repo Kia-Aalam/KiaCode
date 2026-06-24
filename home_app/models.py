3e4293d3717b01ba5f881e6b5fbebd321c61a887
Kia Aalam <kiaaalam7@gmail.com>
2026-06-24 16:13:14 +0330

    add models to contact_me

--- home_app/models.py
+++ home_app/models.py
@@ -3,3 +3,21 @@ from django.db import models
 class Footer(models.Model):
     username = models.CharField(max_length=100)
     message = models.TextField()
+    
+class Contact(models.Model):
+    username = models.CharField(max_length=100, null=False)
+    email = models.EmailField(max_length=254, null=False)
+    
+    SUBJECT_CHOICES = [
+        ('Collaboration', 'پیشنهاد همکاری'),
+        ('Course Question', 'سوال درباره دوره‌ها'),
+        ('Content Suggestion', 'پیشنهاد محتوا'),
+        ('Bug Report', 'گزارش مشکل '),
+        ('Just Saying Hi', 'فقط سلام')
+    ]
+    subject = models.CharField(max_length=50, choices=SUBJECT_CHOICES)
+    
+    message = models.TextField(null=False)
+    
+    def __str__(self):
+        return self.username
--- home_app/views.py
+++ home_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from home_app.models import Footer
+from home_app.models import Footer, Contact
 from courses_app.models import Course
 
 def base_page(request):
@@ -19,4 +19,12 @@ def about_me(request):
     return render(request, 'about_me.html')
 
 def contact(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        Contact.objects.create(username=username, email=email, subject=subject, message=message)
+        return redirect('contact')
+        
     return render(request, 'contact.html')
