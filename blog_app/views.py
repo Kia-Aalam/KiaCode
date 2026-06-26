ffb4abe4dfb75fc9f6afa540429f182b07b1ecf6
Kia Aalam <kiaaalam7@gmail.com>
2026-06-26 18:04:22 +0330

    fix detail_blog , blog , base page

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Category, Blog, Detail
 from django.core.paginator import Paginator
+from django.shortcuts import get_object_or_404
 
 def blog(request):
     # Category
@@ -18,17 +19,15 @@ def blog(request):
         'page_obj': page_obj
     })
 
-def blog_detail(request):
-    # Category
-    categories = Category.objects.all()
-    # Blog
-    blogs = Blog.objects.all()
-    blogs.views += 1
-    blogs.save()
-    # Detail
-    details = Detail.objects.all()
+def blog_detail(request, id):
+    blog = get_object_or_404(Blog, id=id)
+    blog.views += 1
+    blog.save()
+    
+    detail = Detail.objects.get(blog=blog)
+    
     return render(request, 'blog_app/blog_detail.html', context={
-        'categories': categories,
-        'blogs': blogs,
-        'details': details
+        'blog': blog,
+        'detail': detail,
+        'category': blog.category
     })
